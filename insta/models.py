3e14f42f61f5efca098331e9e131358003fa84c3
Cecilia Barasa <cecibarasa@gmail.com>
2020-07-02 17:32:41 +0300

    add image class

--- insta/models.py
+++ insta/models.py
@@ -3,5 +3,17 @@ from django.db import models
 # Create your models here.
 class Instalite(models.Model):
     first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_lenght=30)
+    last_name = models.CharField(max_length=30)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        ordering = ['first_name']
+
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    image_caption = models.CharField(max_length=50)
+    likes = models.IntegerField(default=0)       
+
